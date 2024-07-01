008021b0336df525d97e8f1babbb0451a237303d
Michael Oesterle <michaeloesterle01@gmail.com>
2024-07-01 09:24:24 +0200

    Clean up Pearson correlation

--- experiments/pearson_between_feature_activations.py
+++ experiments/pearson_between_feature_activations.py
@@ -1,8 +1,9 @@
-# Get SAE activations via hooks to save time and memory
+# Calculate and store pair-wise Person correlation for SAE features
 #
 # Define a hook that, for each layer, stores model activations of the current batch in
 # a global store. SAE activations are then calculated from the model activations.
-# From the SAE activations, we calculate the Person correlation between features.
+# From the SAE activations, we calculate the Person correlation between features and
+# save them in a file for each pair of layers.
 
 # %%
 # Imports
@@ -14,7 +15,6 @@ from sae_lens import SAE
 import einops
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-import plotly.express as px
 
 
 # %%
@@ -36,7 +36,7 @@ for layer in tqdm(list(range(model.cfg.n_layers))):
     sae, _, _ = SAE.from_pretrained(
         release="gpt2-small-res-jb",
         sae_id=f"blocks.{layer}.hook_resid_pre",
-        device=device
+        device=device,
     )
     sae.eval()  # prevents error if we're expecting a dead neuron mask for who grads
     saes.append(sae)
@@ -56,21 +56,23 @@ token_dataset = tokenize_and_concatenate(
     add_bos_token=prepend_bos,
 )
 
-tokens = token_dataset['tokens']  # 17.5M tokens
+tokens = token_dataset["tokens"]  # 17.5M tokens
 
 
 # %%
 # OPTIONAL: Reduce dataset for faster experimentation
 # tokens = token_dataset['tokens'][:32]  # 4k tokens
-tokens = token_dataset['tokens'][:1024]  # 128k tokens
-# tokens = token_dataset['tokens'][:8192]  # 1M tokens
+# tokens = token_dataset['tokens'][:1024]  # 128k tokens
+tokens = token_dataset["tokens"][:8192]  # 1M tokens
 
 
 # %%
-# Define object for batched co-occurrence computation
+# Define object for batched Pearson correlation computation
 class BatchedPearson:
     def __init__(self, shape):
-        """Calculates the pair-wise Pearson correlation of two tensors that are provided batch-wise.
+        """Calculates the pair-wise Pearson correlation of two tensors that are
+        provided batch-wise. All computations are done element-wise with broadcasting
+        or einsum.
 
         Args:
             shape (Size): Shape of the result.
@@ -85,37 +87,33 @@ class BatchedPearson:
 
         self.sums_1_2 = torch.zeros(shape)
 
-        self.nonzero_counts_1 = torch.zeros(shape[0])
-        self.nonzero_counts_2 = torch.zeros(shape[1])
-
     def process(self, tensor_1, tensor_2):
         self.count += tensor_1.shape[-1]
 
         self.sums_1 += tensor_1.sum(dim=-1)
         self.sums_2 += tensor_2.sum(dim=-1)
 
-        self.sums_of_squares_1 += (tensor_1 ** 2).sum(dim=-1)
-        self.sums_of_squares_2 += (tensor_2 ** 2).sum(dim=-1)
+        self.sums_of_squares_1 += (tensor_1**2).sum(dim=-1)
+        self.sums_of_squares_2 += (tensor_2**2).sum(dim=-1)
 
-        self.sums_1_2 += einops.einsum(tensor_1, tensor_2, 'f1 t, f2 t -> f1 f2')
-
-        self.nonzero_counts_1 += tensor_1.count_nonzero(dim=-1)
-        self.nonzero_counts_2 += tensor_2.count_nonzero(dim=-1)
+        self.sums_1_2 += einops.einsum(tensor_1, tensor_2, "f1 t, f2 t -> f1 f2")
 
     def finalize(self):
         means_1 = self.sums_1 / self.count
         means_2 = self.sums_2 / self.count
 
-        # Compute the covariance and variances
-        covariances = (self.sums_1_2 / self.count) - einops.einsum(means_1, means_2, 'f1, f2 -> f1 f2')
+        # Compute the covariance, variances, and standard deviations
+        covariances = (self.sums_1_2 / self.count) - einops.einsum(
+            means_1, means_2, "f1, f2 -> f1 f2"
+        )
 
-        variances_1 = (self.sums_of_squares_1 / self.count) - (means_1 ** 2)
-        variances_2 = (self.sums_of_squares_2 / self.count) - (means_2 ** 2)
+        variances_1 = (self.sums_of_squares_1 / self.count) - (means_1**2)
+        variances_2 = (self.sums_of_squares_2 / self.count) - (means_2**2)
 
         stds_1 = torch.sqrt(variances_1).unsqueeze(1)
         stds_2 = torch.sqrt(variances_2).unsqueeze(0)
 
-        # Compute the Pearson correlation coefficient
+        # Compute the Pearson correlation coefficients
         correlations = covariances / stds_1 / stds_2
 
         return correlations
@@ -132,35 +130,27 @@ def retrieval_hook(activations, hook):
     layer = hook.layer()
 
     sae_activations[layer] = einops.rearrange(
-        saes[layer].encode(activations),
-        'batch seq features -> features (batch seq)'
+        saes[layer].encode(activations), "batch seq features -> features (batch seq)"
     )
 
 
-model.add_hook(lambda name: name.endswith('.hook_resid_pre'), retrieval_hook)
+model.add_hook(lambda name: name.endswith(".hook_resid_pre"), retrieval_hook)
 
+layers = list(range(model.cfg.n_layers))
+for layer_1, layer_2 in zip(layers, layers[1:]):
+    print(f"Computing correlation matrix between layers {layer_1} and {layer_2}:")
 
-# Define layers and features for co-occurrence calculation
-layer_1, number_of_features_1 = 6, 24576
-layer_2, number_of_features_2 = 7, 24576
+    aggregator = BatchedPearson((d_sae, d_sae))
+    with torch.no_grad():
+        for batch_tokens in tqdm(data_loader):
+            model.run_with_hooks(batch_tokens)
 
+            # Now we can use sae_activations
+            aggregator.process(sae_activations[layer_1], sae_activations[layer_2])
 
-aggregator = BatchedPearson((number_of_features_1, number_of_features_2))
-with torch.no_grad():
-    for batch_tokens in tqdm(data_loader):
-        model.run_with_hooks(batch_tokens)
+        pearson_correlations = aggregator.finalize()  # (d_sae, d_sae)
 
-        # Now we can use sae_activations
-        aggregator.process(
-            sae_activations[layer_1, :number_of_features_1],
-            sae_activations[layer_2, :number_of_features_2]
-        )
-
-    pearson_correlations = aggregator.finalize()
-
-# `pearson_correlations` is now a (number_of_features_1, number_of_features_2) tensor
-# that stores the correlation values for each pair of features
-
-
-# %%
-px.histogram(pearson_correlations[0][pearson_correlations[0] > 0].cpu(), title='Histogram of Pearson correlations from 6/0 to 7/:')
+    with open(
+        f"res_jb_sae_feature_correlation_pearson_{layer_1}_{layer_2}.pt", "wb"
+    ) as f:
+        torch.save(pearson_correlations, f)
