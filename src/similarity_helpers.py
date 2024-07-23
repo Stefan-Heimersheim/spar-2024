fe0780383cd0c7d362692226e5e2dcdc5cd78cea
Michael Oesterle <michaeloesterle01@gmail.com>
2024-07-23 20:16:07 +0200

    Add filename generator function for similarity measure artefacts

--- src/similarity_helpers.py
+++ src/similarity_helpers.py
@@ -28,4 +28,39 @@ def clamp_low_values(arr, threshold):
 
 def save_compressed(arr, filename):
     np.savez_compressed(filename, arr)
-# %%
+
+
+def get_n_token_description(n_tokens: int) -> str:
+    def is_power_of_2(x):
+        return (x != 0) and (x & (x - 1)) == 0
+
+    if is_power_of_2(n_tokens):
+        if n_tokens >= 2**30:
+            return f"{n_tokens // 2**30}G"
+        elif n_tokens >= 2**20:
+            return f"{n_tokens // 2**20}M"
+        elif n_tokens >= 2**10:
+            return f"{n_tokens // 2**10}k"
+        else:
+            return str(n_tokens)
+    else:
+        if n_tokens >= 1_000_000_000:
+            return f"{n_tokens / 1_000_000_000:.1f}B".rstrip('0').rstrip('.')
+        elif n_tokens >= 1_000_000:
+            return f"{n_tokens / 1_000_000:.1f}M".rstrip('0').rstrip('.')
+        elif n_tokens >= 1_000:
+            return f"{n_tokens / 1_000:.1f}k".rstrip('0').rstrip('.')
+        else:
+            return str(n_tokens)
+
+
+def get_filename(measure_name: str, activation_threshold: float, clamping_threshold: float, n_tokens: int | str, first_layer: int = None, sae_name: str = 'res_jb_sae') -> str:
+    if type(n_tokens) is int:
+        n_tokens = get_n_token_description(n_tokens)
+
+    filename = f'{sae_name}_feature_similarity_{measure_name}_{n_tokens}_{activation_threshold:.1f}_{clamping_threshold:.1f}'
+
+    if first_layer is not None:
+            filename += f'_{first_layer}'
+
+    return filename
