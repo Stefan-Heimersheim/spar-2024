b9288da53a9456a5c456f661f223e31adfdef86d
Michael Oesterle <michaeloesterle01@gmail.com>
2024-09-04 23:19:31 +0200

    Create and run experiment to determine optimal pass-through threshold

--- experiments/pass_through_analysis.py
+++ experiments/pass_through_analysis.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import sys
 import plotly.graph_objects as go
+import pickle
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 
@@ -31,7 +32,7 @@ np.nan_to_num(similarities, copy=False)
 
 
 # %%
-pass_through_threshold = 0.4
+pass_through_threshold = 0.9
 
 forward_max = similarities.max(axis=2)
 forward_pass_through = (forward_max >= pass_through_threshold).sum(axis=1)
@@ -75,3 +76,54 @@ if not os.path.exists(output_folder):
     os.mkdir(output_folder)
 
 fig.write_image(f"{output_folder}/{sae_name}_pass_through_features_{measure_name}_{tokens}_{pass_through_threshold:.1f}.png")
+
+
+# %%
+# Find optimal pass-through threshold by performing a binary search, 
+# comparing explanations for any given threshold
+measure_name = 'pearson_correlation'
+activation_threshold = None
+tokens = '10M'
+
+# Load (unclamped) similarity matrix
+print('Loading similarity matrix...')
+input_files = [f'{artefacts_folder}/similarity_measures/{measure_name}/.unclamped/{get_filename(measure_name, input_artefact, activation_threshold, None, tokens, layer)}.npz' for layer in range(n_layers - 1)]
+similarities = load_similarity_data(input_files)
+np.nan_to_num(similarities, copy=False)
+
+print('Loading explanations...')
+with open(f'../artefacts/explanations/{sae_name}_explanations.pkl', 'rb') as f:
+    explanations = pickle.load(f)
+
+
+# %%
+number_of_samples = int(input('How many samples would you like to see at each step?'))
+
+print('Starting binary search...')
+lower, upper, threshold = 0.5, 1, 0.75
+while True:
+    print(f'Current threshold: {threshold}')
+
+    # Create, sample, and output examples
+    pass_through_features = np.array(np.where(similarities >= threshold)).T
+    print(f'{len(pass_through_features)} feature pairs are above the threshold.\n')
+    samples = pass_through_features[np.random.choice(len(pass_through_features), number_of_samples, replace=False)]
+
+    print('\n\n'.join([f'{l}_{f1}: {explanations[l][f1]}\n{l+1}_{f2}: {explanations[l+1][f2]}' for l, f1, f2 in samples]))
+    
+    answer = input('Do all of the above examples qualify as pass-through features?')
+
+    if answer == 'yes':
+        upper = threshold
+    elif answer == 'no':
+        lower = threshold
+    elif answer == '':
+        break
+
+    threshold = (upper + lower) / 2
+
+    print('\n')
+
+print(f'Narrowed threshold down to [{lower}, {upper}].')
+
+# %%
